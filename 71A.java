a68a3e840508946237438bf1f510aef6d3abe29b
Gokul Suresh <gokul.suresh24@gmail.com>
2016-07-16 12:25:17 -0400

    completed 71A

---
+++ 71A.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class words {
+
+	public static String tooLong(String s) {
+		if (s.length() > 10) {
+			StringBuffer newString = new StringBuffer();
+			newString.append(s.charAt(0));
+			newString.append(s.length() - 2);
+			newString.append(s.charAt(s.length() - 1));
+			return newString.toString();
+		}
+
+		else {
+			return s;
+		}
+	}
+
+	public static void main(String args[]) {
+		Scanner scan = new Scanner(System.in);
+		String number = scan.nextLine();
+		int n = Integer.parseInt(number);
+		for (int i = 0; i < n; i++) {
+			String s = scan.nextLine();
+			System.out.println(tooLong(s));
+		}
+	}
+}
